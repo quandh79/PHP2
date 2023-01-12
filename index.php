f1066ef0cab881d201a2c5bcf636b3db25bb5918
quandh79 <dohongquantc@gmail.com>
2023-01-12 09:20:30 +0700

    1

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+<?php include("head.php") ?>
+</head>
+
+<body>
+<!-- Page Preloder -->
+<div id="preloder">
+    <div class="loader"></div>
+</div>
+
+<!-- Humberger Begin -->
+<?php include("comportent/humberger.php") ?>
+<!-- Humberger End -->
+
+<!-- Header Section Begin -->
+<?php include("comportent/header_section.php") ?>
+<!-- Header Section End -->
+
+<!-- Hero Section Begin -->
+<?php include("comportent/hero_section.php") ?>
+<!-- Hero Section End -->
+
+<!-- Categories Section Begin -->
+<?php include("comportent/categories_section.php") ?>
+<!-- Categories Section End -->
+
+<!-- Featured Section Begin -->
+<?php include("comportent/featured_section.php") ?>
+<!-- Featured Section End -->
+
+<!-- Banner Begin -->
+<?php include("comportent/banner.php") ?>
+<!-- Banner End -->
+
+<!-- Latest Product Section Begin -->
+<?php include("comportent/product_section.php") ?>
+<!-- Latest Product Section End -->
+
+<!-- Blog Section Begin -->
+<?php include("comportent/section.php") ?>
+<!-- Blog Section End -->
+
+<!-- Footer Section Begin -->
+<?php include("comportent/footer.php") ?>
+<!-- Footer Section End -->
+
+<!-- Js Plugins -->
+<?php include("comportent/js.php") ?>
+
+
+
+</body>
+
+</html>
---
+++ shop-grid.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <?php include ("head.php")?>
+</head>
+
+<body>
+<!-- Page Preloder -->
+<div id="preloder">
+    <div class="loader"></div>
+</div>
+
+<!-- Humberger Begin -->
+<?php include("comportent/humberger.php") ?>
+<!-- Humberger End -->
+
+<!-- Header Section Begin -->
+<?php include("comportent/header_section.php") ?>
+<!-- Header Section End -->
+
+<!-- Hero Section Begin -->
+<?php include("comportent/hero_section.php") ?>
+<!-- Hero Section End -->
+
+<!-- Breadcrumb Section Begin -->
+<?php include("comportent/breadcrumb_section.php") ?>
+<!-- Breadcrumb Section End -->
+
+<!-- Product Section Begin -->
+<?php include("comportent/product_section.php") ?>
+<!-- Product Section End -->
+
+<!-- Footer Section Begin -->
+<?php include("comportent/footer.php") ?>
+<!-- Footer Section End -->
+
+<!-- Js Plugins -->
+<?php include("comportent/js.php") ?>
+
+
+
+</body>
+
+</html>
